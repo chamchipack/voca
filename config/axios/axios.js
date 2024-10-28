d8986c38edef0773ea075e6c76ab088426934378
chanik <chanik@chanik.gractor.com>
2024-10-28 17:09:39 +0900

    good

--- config/axios/axios.js
+++ config/axios/axios.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
-import PocketBase from 'pocketbase'
+import axios from "axios";
+import PocketBase from "pocketbase";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: "http://127.0.0.1:8050",
   timeout: 5000,
 });
 
@@ -11,77 +11,77 @@ const getData = async (url) => {
     // const response = await axiosInstance.get('/api/collections/members/records');
     const response = await axiosInstance.get(url, {
       headers: {
-        'zoe-token': process.env.NEXT_PUBLIC_API_KEY,
-        'Content-Type': 'application/json',
-      }
+        "zoe-token": process.env.NEXT_PUBLIC_API_KEY,
+        "Content-Type": "application/json",
+      },
     });
 
-    return response.data
+    return response.data;
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const getPocket = async (url) => {
   try {
     // const response = await axiosInstance.get('/api/collections/members/records');
     const response = await axiosInstance.get(url);
 
-    return response.data
+    return response.data;
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const postData = async (url, data) => {
   try {
     const response = await axiosInstance.post(url, data, {
       headers: {
-        'zoe-token': process.env.NEXT_PUBLIC_API_KEY,
-        'Content-Type': 'application/json',
-      }
+        "zoe-token": process.env.NEXT_PUBLIC_API_KEY,
+        "Content-Type": "application/json",
+      },
     });
-    return response.data
+    return response.data;
   } catch (error) {
     console.error(error);
   }
-}
+};
 
 const putPocket = async (url, data) => {
-  const { id = '', ...rest } = data
+  const { id = "", ...rest } = data;
   try {
-    const response = await axiosInstance.patch(url + '/' + id, rest, {
+    const response = await axiosInstance.patch(url + "/" + id, rest, {
       headers: {
-        'zoe-token': process.env.NEXT_PUBLIC_API_KEY,
-        'Content-Type': 'application/json',
-      }
+        "zoe-token": process.env.NEXT_PUBLIC_API_KEY,
+        "Content-Type": "application/json",
+      },
     });
-    return response.data
+    return response.data;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 const deletePocket = async (url) => {
   try {
     const response = await axiosInstance.delete(url, {
       headers: {
-        'zoe-token': process.env.NEXT_PUBLIC_API_KEY,
-        'Content-Type': 'application/json',
-      }
+        "zoe-token": process.env.NEXT_PUBLIC_API_KEY,
+        "Content-Type": "application/json",
+      },
     });
-    return response.data
+    return response.data;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 const result = {
   get: getData,
   post: postData,
   put: putPocket,
   delete: deletePocket,
-  getPocket
-}
+  getPocket,
+};
 
 export default result;
